457f43e640c915483b8474ca97c765e24fbc066e
EasyWin6 <sunshineregi888@gmail.com>
2020-01-29 23:09:23 +0200

    11

--- src/mx_print_long.c
+++ src/mx_print_long.c
@@ -15,7 +15,7 @@ void mx_print_long(t_main *m_ls) {
         mx_printstr(m_ls->pointer[i].group_name);
         print_spaces(m_ls->max_gid_size + 2 - mx_strlen(m_ls->pointer[i].group_name));
         mx_printint(m_ls->pointer[i].stat.st_size);
-        print_spaces(m_ls->max_byte_size + 1 - (mx_strlen(mx_itoa(m_ls->pointer[i].stat.st_size))));
+        print_spaces(m_ls->max_byte_size - (mx_strlen(mx_itoa(m_ls->pointer[i].stat.st_size))));
 //        if (S_ISCHR(m_ls->pointer[i].stat.st_mode) || S_ISBLK(m_ls->pointer[i].stat.st_mode)) {
 //            mx_printint(m_ls->pointer[i].stat.st_dev);
 //            mx_printstr(", ");
