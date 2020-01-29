761c613a00a494b3864332841086a7bab8e5afc3
EasyWin6 <sunshineregi888@gmail.com>
2020-01-29 23:07:02 +0200

    11

--- src/mx_print_long.c
+++ src/mx_print_long.c
@@ -15,9 +15,19 @@ void mx_print_long(t_main *m_ls) {
         mx_printstr(m_ls->pointer[i].group_name);
         print_spaces(m_ls->max_gid_size + 2 - mx_strlen(m_ls->pointer[i].group_name));
         mx_printint(m_ls->pointer[i].stat.st_size);
-        mx_printstr("  ");
+        print_spaces(m_ls->max_byte_size + 1 - (mx_strlen(mx_itoa(m_ls->pointer[i].stat.st_size))));
+//        if (S_ISCHR(m_ls->pointer[i].stat.st_mode) || S_ISBLK(m_ls->pointer[i].stat.st_mode)) {
+//            mx_printint(m_ls->pointer[i].stat.st_dev);
+//            mx_printstr(", ");
+//            mx_printint(m_ls->pointer[i].stat.st_size);
+//        }
+//        else
+//            print_spaces(2);
+//        mx_printstr(mx_human_read_size(m_s->pointer_s[i].stat.st_size));
+//        mx_printint(m_ls->pointer[i].stat.st_size);
+        print_spaces(1);
         mx_print_specific_time(m_ls->pointer[i].stat.st_mtimespec.tv_sec);
-        mx_printstr("  ");
+        print_spaces(1);
         mx_printstr(m_ls->pointer[i].file_name);
         mx_printstr("\n");
     }
